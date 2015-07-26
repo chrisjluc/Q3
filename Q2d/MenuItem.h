3aaf28d45cad9f9cd51e54dec4ce06dd50fef706
Christopher Luc <chris.luc.dev@gmail.com>
2015-07-25 22:09:17 -0400

    Added Q2d

---
+++ Q2d/MenuItem.h
@@ -0,0 +1,37 @@
+#ifndef MENU_ITEM_H
+#define MENU_ITEM_H
+
+#include <iostream>
+#include <string>
+
+#include "MenuComponent.h"
+
+class MenuItem : public MenuComponent {
+public:
+    // constructor
+    MenuItem(const std::string, float, int, bool);
+
+    // implement leaf operations
+    virtual bool isVeggie() const;
+
+    virtual float price() const;
+
+    virtual void priceIs(float p);
+
+    virtual int calories() const;
+
+    virtual void caloriesIs(int c);
+
+    virtual bool isLeaf() const { return true; };
+
+protected:
+    virtual void print(std::ostream &) const;
+
+private:
+    float price_;
+    int calories_;
+    bool isVeggie_;
+};
+
+
+#endif
